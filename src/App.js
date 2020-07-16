33dad2e6806f89691d5ec8a5a4b175799fe3e7bc
haileyhansard <haileyhansard@gmail.com>
2020-07-16 16:46:56 -0700

    Meeting MVP

--- src/App.js
+++ src/App.js
@@ -7,26 +7,42 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+
+//Step 3, import ProductContext into our App
+import { ProductContext } from './contexts/ProductContext';
+//Step 5b import CartContext into our App
+import { CartContext } from './contexts/CartContext';
+
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
+	//Step 1
 	const addItem = item => {
-		// add the given item to the cart
+		setCart(
+			[...cart, item]
+		)
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			{/* // Step 3b, wrap all components and routes in ProductContext.Provider */}
+			{/* // Step 5c wrap all components and routes in CartContext.Provider, but let ProductContext.Provider be the root */}
+			<ProductContext.Provider value={{ products, addItem }}> 
+				<CartContext.Provider value={{ cart }}>
+					<Navigation />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+					{/* Routes */}
+					<Route exact path="/">
+						<Products />
+					</Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+					<Route path="/cart">
+						<ShoppingCart />
+					</Route>
+				</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
---
+++ src/contexts/CartContext.js
@@ -0,0 +1,4 @@
+//Step 5
+import { createContext } from 'react';
+
+export const CartContext = createContext();
